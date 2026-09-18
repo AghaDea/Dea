ab654e0b81a306248b2a100db012fd64ab9fe8f0
Dea <mohamadreza13885233@gmail.com>
2026-09-19 00:52:35 +0330

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
-// ================= SUPABASE CONFIG =================
-// مقادیر پروژه Supabase خودت را اینجا قرار بده.
+// فقط این دو مقدار را با اطلاعات Supabase خودت پر کن.
 window.BANANA_CONFIG = {
   SUPABASE_URL: "https://cacqqiysixpupfupqyvy.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_ewjtSc_Qg2ntxZpI9YfnMA_0XwuPF0y"
